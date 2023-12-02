70eccae4558afe802198a83cba0799e20791c22c
MichalKinas <michal.kinas@hyland.com>
2023-12-02 15:39:53 +0100

    Add Angular Material, tabs in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatTabsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
