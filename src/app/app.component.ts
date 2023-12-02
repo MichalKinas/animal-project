5287df32a2e53d99183b6c026b448ac76d48c9dd
MichalKinas <michal.kinas@hyland.com>
2023-12-02 17:20:54 +0100

    Use button toggle instead of tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonToggleChange, MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatTabsModule],
+  imports: [CommonModule, RouterOutlet, MatButtonToggleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'animal-project';
+
+  onFactsSourceChange(event: MatButtonToggleChange) {
+    console.log(event);
+  }
 }
